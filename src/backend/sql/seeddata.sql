7a43b6ddab825bca718f9807b16f3c563e958bb9
Cory Hall <cory_hall88@yahoo.com>
2023-07-25 01:29:47 -0400

    adding seed data sql

---
+++ src/backend/sql/seeddata.sql
@@ -0,0 +1,9 @@
+Create Table dbo.Students
+(
+StudentName nvarchar(100),
+StudentGrade nvarchar(100)
+)
+
+INSERT INTO Students (StudentName, StudentGrade) 
+Values('Cory Hall', '9th Grade')
+,('Cory Hall', '10th Grade')
