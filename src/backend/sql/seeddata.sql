75d7a951f7478d5aaeba59606c569cf2275b31f1
Cory Hall <cory_hall88@yahoo.com>
2023-07-30 21:39:18 -0400

    updating sql script

--- src/backend/sql/seeddata.sql
+++ src/backend/sql/seeddata.sql
@@ -1,9 +1,12 @@
-Create Table dbo.Students
+DROP TABLE IF EXISTS [dbo].[Students];
+
+Create Table [dbo].[Students]
 (
 StudentName nvarchar(100),
 StudentGrade nvarchar(100)
 )
 
 INSERT INTO Students (StudentName, StudentGrade) 
-Values('Cory Hall', '9th Grade')
-,('Cory Hall', '10th Grade')
+Values('Jakora Hall', '8th Grade')
+,('Jakyra Hall', '5th Grade')
+,('Cory Hall, Jr.', "Kindergarten")
